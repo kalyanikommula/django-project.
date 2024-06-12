2358dfe52c5b170b5d244055adc416d551f1e99c
kalyani kommula <kalyanib.tech505@gmail.com>
2024-06-12 11:50:54 +0000

    edited

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request)
-    returb HttpResponse("Hello, world!")
+def index(request):
+    return HttpResponse("Hello, world!")
